46397c35714d3d002f6c35e44808bbed37b2f55a
Donato, John Darev A <155620476+FIT-DONATO@users.noreply.github.com>
2024-06-21 12:22:17 +0800

    Create pracjs.js

---
+++ pjs/pracjs.js
@@ -0,0 +1,86 @@
+
+function compute(){
+
+    let rad = document.getElementById("radius").value;
+    let area = rad * rad * 3.14;
+
+    document.getElementById("area").textContent = 
+    `The area of a circle with radius of ${rad} is ${area} cm^2.`
+}
+
+// console.log(rad);
+
+// let num1 = 0;
+// let surName;
+// const PI = 3.1416;
+
+// console.log(surname);
+// console.log(typeof(surName));
+
+// console.log(num1);
+// console.log(typeof(num1));
+
+// console.log(PI);
+// console.log(typeof(PI));
+
+// alert(email);
+
+// let email = 'yes@yahoo.com';
+// let vocalist = 'yes';
+// let firstName = 'John';
+// let lastName = 'Donato';
+// let num = 10;
+// let allCaps = firstName.toUpperCase();
+// let sliced = email.slice(0,5);
+
+// console.log(sliced);
+
+// console.log(email.indexOf('@'));
+
+// console.log(allCaps);
+
+// console.log(email);
+// console.log(typeof(email));
+// console.log(vocalist);
+// console.log(typeof(vocalist));
+
+// console.log(firstName.length);
+// console.log(firstName + ' ' + lastName);
+
+// template string using back tick
+// console.log(`The vocalist of PNE is ${firstName} ${lastName}.`)
+// console.log(`The vocalist of PNE is ${firstName} ${num}.`)
+// console.log(num + 2)
+
+// let radius = 10;
+// const PI = 3.14;
+
+// let answer = radius ** 3 * PI;
+
+// console.log(answer);
+// console.log(`The answer of this calculation is ${answer}.`);
+
+// let chr = [];
+
+// chr[0] ='mario';
+// chr[1] ='luigi';
+// chr[2] ='peach';
+// chr[3] = 100;
+// chr[4] = true;
+
+// chr.push('koopa');
+// chr.push('toad');
+
+// console.log(chr[0]);
+// console.log(chr[1]);
+// console.log(chr[2]);
+// console.log(chr[3]);
+// console.log(chr[4]);
+// console.log(chr[5]);
+// console.log(chr[6]);
+
+// boolean
+// let num1 =5;
+// let num2 = 10;
+
+// console.log(num1 < num2);
